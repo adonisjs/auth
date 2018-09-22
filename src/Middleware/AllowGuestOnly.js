c999ad6a408778445a4e13a054838ee42c7a8790
Romain Lanz <romain.lanz@slynova.ch>
2018-09-22 10:26:31 +0200

    fix(middleware): unpack request from ctx

--- src/Middleware/AllowGuestOnly.js
+++ src/Middleware/AllowGuestOnly.js
@@ -3,11 +3,11 @@
 const GE = require('@adonisjs/generic-exceptions')
 
 class AllowGuestOnly {
-  async handle ({ auth, response }, next) {
+  async handle ({ auth, request }, next) {
     try {
       await auth.check()
 
-      throw new GE.HttpException(`Only guest user can access the route ${ctx.request.method()} ${ctx.request.url()}`, 403, 'E_GUEST_ONLY')
+      throw new GE.HttpException(`Only guest user can access the route ${request.method()} ${request.url()}`, 403, 'E_GUEST_ONLY')
     } catch (e) {}
 
     await next()
